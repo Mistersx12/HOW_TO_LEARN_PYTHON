81dd669ba372a0e8a8166181d40da5fbc3d2c89e
mistersx12 <211091il@gmail.com>
2020-01-07 17:43:12 +0100

    chnages

--- FUNCTIONS/FIzzBuzz-Function.py
+++ FUNCTIONS/FIzzBuzz-Function.py
@@ -1,12 +1,12 @@
-def getFizzBuzz(multiples, *args):
-  for i in range(*args):
-    output = ''
-    for multiple in multiples:
-      if i% multiple == 0:
-        output += multiples[multiple]
-    if output == ''
-      output = i
-    print(output)
+def getFizzBuzz(multiples, *args)
+    for i in range(*args):
+        output = ''
+        for multiple in multiples:
+            if i%multiple == 0
+                output += multiples[multiple]
+        if output == '':
+            oupput = i
+        print(output)
 
-multiples = {3:'Fizz',5:'Buzz'}
-getFizzBuzz(multiples, 1,101)
+    multiples = {3:'Fizz',5:'Buzz'}
+    getFizzBuzz(multiples, 1, 101)
