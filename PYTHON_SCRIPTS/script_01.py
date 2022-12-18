c870472316ebe51063d986ac400c36620bdabc6d
Mx12 <42295944+Mistersx12@users.noreply.github.com>
2022-12-18 10:53:09 +0100

    Create script_01.py

---
+++ PYTHON_SCRIPTS/script_01.py
@@ -0,0 +1,16 @@
+#simple Python script 
+
+def main():
+  print("Welcome to the 'How to Learn Python' project!")
+  print("Here, you'll find a variety of resources to help you get started with learning Python.")
+
+if __name__ == '__main__':
+  main()
+
+#This script defines a main function that prints a welcome message to the user.
+#The if __name__ == '__main__' block at the bottom of the script is a common pattern in Python that allows the script to be run as a standalone program,
+#but also imported and used as a module in other programs.
+
+#To run this script, you can simply use the Python interpreter:
+
+python learn_python.py
