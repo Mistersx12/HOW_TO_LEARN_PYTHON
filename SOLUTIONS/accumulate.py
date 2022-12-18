c4590a7fad1bb18a1dc2d4600cd06c3b394ab476
Mx12 <42295944+Mistersx12@users.noreply.github.com>
2022-12-18 10:46:00 +0100

    Update accumulate.py

--- SOLUTIONS/accumulate.py
+++ SOLUTIONS/accumulate.py
@@ -1,3 +1,17 @@
+#Implement the accumulate operation, which, given a collection and an operation to perform on each element of the collection,
+#returns a new collection containing the result of applying that operation to each element of the input collection.
+
+#Given the collection of numbers:
+
+#1, 2, 3, 4, 5
+#And the operation:
+
+#square a number (x => x * x)
+#Your code should be able to produce the collection of squares:
+
+#1, 4, 9, 16, 25
+#~~Check out the test suite to see the expected function signature.~~
+
 def accumulate(collection, operation):
   return [operation(x) for x in collection]
 
